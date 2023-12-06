53ef7ae9780b5ef2ef9f06b556ff72557c1a7e62
Yanter44 <zhmih44@gmail.com>
2023-12-06 20:59:59 +0300

    Onet_Ne_Hochet_Mne_Rasskazat_Pro_Telnet

--- Entyties/Book.cs
+++ Entyties/Book.cs
@@ -1,12 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace POPITKAPOPASTNASTAJIROVKU.Entyties
 {
     public class Book
     {
-       [Key]
-       [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        [Key]
+       
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [JsonIgnore]
         public int Id { get; set; } 
         public string Description { get; set; }
         public int ISBN { get; set; }
